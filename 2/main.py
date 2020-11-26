6eda8daf7cb891d3b0515452dfa21e6720782965
Daniyar <70882045+daniyar0@users.noreply.github.com>
2020-11-26 15:55:41 +0500

    Update main.py

--- 2/main.py
+++ 2/main.py
@@ -1,5 +1,16 @@
+#def Values():
+ #   a, b, c, d, f = int(input()), int(input()), int(input()), int(input()), int(input())
+  #  print('Сумма введенных значений:', a+b+c+d+f)
+#print('Введите значения:')
+#Values()
 def Values():
-    a, b, c, d, f = int(input()), int(input()), int(input()), int(input()), int(input())
-    print('Сумма введенных значений:', a+b+c+d+f)
-print('Введите значения:')
+    sum = 0
+    b = []
+    for i in range(5):
+        a = int(input('Введите значения:'))
+        print(a)
+        b.append(a)
+    for i in b:
+            sum += i
+    print(sum)
 Values()
